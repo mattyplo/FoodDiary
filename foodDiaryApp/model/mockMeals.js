5d4313927e51142dde7cd1370064e1ac46f38228
mattyplo <matthew.ploszaj@seattlecentral.edu>
2019-02-22 14:52:22 -0800

    dates, meals, and food items are now displayed on page with data pulled from mock data

--- foodDiaryApp/model/mockMeals.js
+++ foodDiaryApp/model/mockMeals.js
@@ -3,39 +3,39 @@
 var mockMeals = [
   {
     "2019-02-11" : {
-      'breakfast' : [{
+      'breakfast' : {
         'foodItem1' : 'eggs'
-      }],
-      'lunch' : [{
+      },
+      'lunch' : {
         'foodItem1' : 'Peanut Butter and Jelly Sandwich',
         'foodItem2' : 'Coca-cola'
-      }],
-      'dinner' : [{
+      },
+      'dinner' : {
         'foodItem1' : 'Steak',
         'foodItem2' : 'Mash Potatoes',
         'foodItem3' : 'Green Beans'
-      }],
-      'snack' : [{
+      },
+      'snack' : {
         'foodItem1' : 'Peanuts'
-      }]
+      }
     },
   "2019-02-12" : {
-      'breakfast' : [{
+      'breakfast' : {
         'foodItem1' : 'Oatmeal',
         'foodItem2' : 'Coffee'
-      }],
-      'lunch' : [{
+      },
+      'lunch' : {
         'foodItem1' : 'Chicken Salad',
         'foodItem2' : 'Coca-cola'
-      }],
-      'dinner' : [{
+      },
+      'dinner' : {
         'foodItem1' : 'Pork Chops',
         'foodItem2' : 'Collard Greens',
         'foodItem3' : 'Fava Beans'
-      }],
-      'snack' : [{
+      },
+      'snack' : {
         'foodItem1' : 'Fruit Roll Up'
-      }]
+      }
     }
   }]
 
