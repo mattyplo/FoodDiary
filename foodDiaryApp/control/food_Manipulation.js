0394cb61ed12694ec4ac9383c9fb00a7488a0f87
YunFSui <syatwork@outlook.com>
2019-02-25 18:16:51 -0800

    made food_add into a module

---
+++ foodDiaryApp/control/food_Manipulation.js
@@ -0,0 +1,55 @@
+// TL;DR foodManipulation({foodName: , gPerServing: , cPerGram: })
+
+let sql = require('../model/db_connection');
+
+function foodManipulation(food) {
+
+    // Update Food With Given FoodID
+    let updateWithID = "UPDATE foods SET "
+                    + "GramsPerServing = "
+                    + food.gPerServing
+                    + ", CaloriesPerGram = "
+                    + food.cPerGram
+                    + " WHERE FoodID = "; 
+
+    function updateFood(food, foodID) {
+        //updateUsing foodID from db
+        sql(updateWithID + foodID, (err, result) => {
+            if (err) throw err;
+            console.log(result);
+        }) ;
+    }
+
+    // Add Food
+    let query_addFood = "INSERT INTO Foods"
+                + "(FoodName, GramsPerServing, CaloriesPerGram) "
+                + "VALUES ("
+                + "'" + food.foodName + "', "
+                + food.gPerServing + ", "
+                + food.cPerGram + ")";
+
+    function addFood(food) {
+        sql(query_addFood, (err, result) => {
+            if (err) throw err;
+            console.log(result);
+        })
+    }
+
+    // Take a food object, use foodName to find if food exist, if exist update, if not add.
+    let findFoodID = "SELECT FoodID FROM foods WHERE FoodName = '"
+                    + food.foodName +"';";
+
+    sql(findFoodID, (err, result) => {
+
+        if (err) throw err;
+        if (typeof result === 'undefined' || result.length == 0){
+            console.log("undefined");
+            addFood(food);
+        } else {
+            var foodID = result[0].FoodID;
+            updateFood(food, foodID);
+        }
+    });
+}
+
+module.exports = foodManipulation;
