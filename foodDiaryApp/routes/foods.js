acef8ab777caaac9b6eebbad33bb671b1ea2c63c
YunFSui <syatwork@outlook.com>
2019-03-01 18:48:32 -0800

    prevent empty food query to be send to db

--- foodDiaryApp/routes/foods.js
+++ foodDiaryApp/routes/foods.js
@@ -1,29 +1,29 @@
 var express = require('express');
 let fm = require('../control/food_Manipulation')
 var router = express.Router();
-var app = express();
-const util = require('util');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  let food = req.query;
-  console.log(food.foodName);
-  fm(food);
+
   res.render('foods', {
     title:'FOODS',
-  
 
-   
+    
   });
 
-//router.get('/', function(req, res){
-   /* let food = req.query;
-    console.log(food.foodName);
-    fm(food);*/
-//});
-
-
+  let food = req.query;
+  console.log('food is ' + food.foodName);
+  fm(food);
 });
+
+// router.get('/', function(req, res, next) {
+
+//   let food = req.query;
+//   fm(food);
+
+// });
+
 
 
 
