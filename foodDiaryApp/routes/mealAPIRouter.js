c83994149a29ecec0c787bc6f1f133934b9f142a
mattyplo <matthew.ploszaj@seattlecentral.edu>
2019-03-03 10:55:30 -0800

    am able to call mealsFoodsID api with a parameter hardcoded and receive a response without errors

--- foodDiaryApp/routes/mealAPIRouter.js
+++ foodDiaryApp/routes/mealAPIRouter.js
@@ -12,4 +12,15 @@ router.get('/all', function(req, res, next) {
   });
 });
 
+router.get('/mealsFoodsID/:mealID', function(req, res, mnext) {
+  var query = "SELECT FoodID FROM MealsFoods WHERE MealID =" + req.params.mealID;
+  //console.log(req.params.mealID);
+  db(query, (error, result, fields) => {
+    if (error) {
+      res.status(500).send(error);
+    }
+    res.send(result);
+  })
+})
+
 module.exports = router;
