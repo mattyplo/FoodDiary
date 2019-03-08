49852c0c10201a3f5ffdbb31d5825d182e8526bc
mattyplo <matthew.ploszaj@seattlecentral.edu>
2019-03-08 11:32:52 -0800

    a form select element with all the meals listed as options has been added to the edit form in the journal page

--- foodDiaryApp/public/js/editMeal.js
+++ foodDiaryApp/public/js/editMeal.js
@@ -2,17 +2,39 @@ $(document).ready(function() {
   
   // open the form when button is clicked
   function openForm() {
-    document.getElementById("editMealForm").style.display = "block";
+    document.getElementById("editMealDiv").style.display = "block";
+    displayMeals();
+  }
+  
+  function displayMeals() {
+    
+    var selectMenu = "<select name='meal'>";
+    $.getJSON("/api/v1/meals/all", (result)=>{
+      $.each(result, (i, field) => {
+        selectMenu += "<option value='" + field.MealID + "'> " + field.MealDate + "</option>";
+        /*
+          MealDate
+          MealID
+          MealTypeID
+        */
+    
+      });
+      console.log(selectMenu);
+      selectMenu += "</select>";
+      $("#editMealForm").append(selectMenu);
+      //outputMealData(meals);
+   
+    });
   }
   
   // close the form when button is clicked
   function closeForm() {
-    document.getElementById("editMealForm").style.display = "none";
+    document.getElementById("editMealDiv").style.display = "none";
   }
   
   var editMealButton = document.getElementById("editMeal");
-  var closeFormButton = document.getElementById("closeFormButton");
+  var closeFormButton = document.getElementById("closeEditFormButton");
   
   editMealButton.addEventListener("click", openForm, false);
   closeFormButton.addEventListener("click", closeForm, false);
-}
+});
