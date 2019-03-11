7b31d429207ad89b014c6cea78fdfd54fe4ccbea
mattyplo <matthew.ploszaj@seattlecentral.edu>
2019-03-11 09:47:17 -0700

    added hidden fields to capture caloriesPerGram and gramsPerServing to editForm form

--- foodDiaryApp/public/js/editMeal.js
+++ foodDiaryApp/public/js/editMeal.js
@@ -6,33 +6,43 @@ $(document).ready(function() {
     displayMeals();
     document.getElementById('mealSelector').addEventListener('change', selectMeal);
     var foodName;
-    
+    var gramsPerServing;
+    var caloriesPerGram;
+    var mealID;
   }
     
   function selectMeal() {
     var selector = document.getElementById('mealSelector');
-    var value = selector[selector.selectedIndex].value;
+    mealID = selector[selector.selectedIndex].value;
     $.ajax({ 
-      url: "/api/v1/meals/foodInfo/" + value, 
+      url: "/api/v1/meals/foodInfo/" + mealID, 
       dataType: 'json', 
       async: false, 
       success: function(json){   
         foodName = json[0].FoodName;  
+        gramsPerServing = json[0].gramsPerServing;
+        caloriesPerGram = json[0].caloriesPerGram;
       }  
     })
-    console.log(foodName);
     displayTextFields();
   }
   
   // creates a text field, with default value set to food associated with meal chosen from selector option
   function displayTextFields() {
-    var fields = "<input type='text' name='foodName' value='" + foodName + "'><br>";
-    $("#editMealForm").append(fields);
+    var fields = "<input id='foodNameID' type='text' name='foodName' value='" + foodName + "'><br>";
+    var hiddenFields = "<input id='hiddenGramsPerServing' type='hidden' name='gramsPerServing' value='" + gramsPerServing + "'><input id='hiddenCaloriesPerGram' type='hidden' name='caloriesPerGram' value='" + caloriesPerGram + "'>";
+    if(document.getElementById("foodNameID")) {
+      document.getElementById('foodNameID').value = foodName;
+      document.getElementById('hiddenGramsPerServing').value = gramsPerServing;
+      document.getElementById('hiddenCaloriesPerGram').value = caloriesPerGram;
+    } else {
+      $("#editMealForm").append(fields);
+      $("#editMealForm").append(hiddenFields);
+    }
   }
   
   function displayMeals() {
-    
-    //var selectMenu = "<select name='meal' id='mealSelector'>";
+
     $.getJSON("/api/v1/meals/all", (result)=>{
       $.each(result, (i, field) => {
         var selectMenu = "<option value='" + field.MealID + "'> " + field.MealDate + "</option>";
