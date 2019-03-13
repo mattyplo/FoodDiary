7689b7a6bf40dca86254556125bee214e40f7da9
Quincy Powell <quincy.powell@gmail.com>
2019-03-12 20:17:45 -0700

    Login mostly working. The page handoff breaks.

--- foodDiaryApp/app.js
+++ foodDiaryApp/app.js
@@ -12,6 +12,8 @@ var mealAPIRouter = require('./routes/mealAPIRouter');
 var foodsListRouter = require('./routes/foods_list');
 var foodsAPIRouter = require('./routes/foods');
 var foodItemRouter = require('./routes/foods_detail');
+var loginRouter = require('./routes/login');
+var login_failRouter = require('./routes/login_fail');
 
 var app = express();
 
@@ -33,6 +35,8 @@ app.use('/foods', foodsListRouter);
 app.use('/api/v1/food', foodsAPIRouter);
 app.use('/foods/detail', foodItemRouter);
 app.use('/api/v1/meals', mealAPIRouter);
+app.use('/login', loginRouter);
+app.use('/login_fail', login_failRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ foodDiaryApp/routes/login.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../model/db');
+
+/* Access login function */
+router.post('/', function(req, res, next) {
+    var query = "SELECT UserName, Password"
+        + " FROM Users"
+        + " WHERE Users.UserName = '" + req.body.username + "';"
+
+    db.query(query, function(error, result, fields) {
+	if (error) {
+            // Web result of DB error possible?
+	    //res.status(500.send(error));
+	    console.log(error);
+	} else {
+	    if(String(result[0].Password).trim() == String(req.body.password).trim()) {
+		// I believe session code should be added here:
+
+		res.render('journal', {title:'Journal'})
+	    } else {
+		res.render('bad_login', {title:"Login failure."})
+	    }
+	    
+	}
+    });
+});
+
+
+module.exports = router;
+/* maybe possible to redirect to logon page on logon fail?
+router.get('/', function(req, res, next) {
+    res.render('login', { title: 'Login' });
+});
+
+module.exports = router;
+*/
