402bbaed6372a3069a907ad5ef11002bc4ad012f
YunFSui <syatwork@outlook.com>
2019-03-16 17:00:57 -0700

    add response for empty result

--- foodDiaryApp/public/js/foodListGen.js
+++ foodDiaryApp/public/js/foodListGen.js
@@ -1,50 +1,31 @@
-// $(document).ready(function(){
-
-
-
-//   // var searchbtn = document.getElementById('searchbtn');
-//   // searchbtn.addEventListener('click', searchJSON());
-//   // function searchJSON() {
-//   //   console.log('hello foodlistgen');
-//   // $.getJSON('/api/v1/food/search?foodName=apple', (results) => {
-//   //   console.log('hello from inside getJSON');
-//   //   var SR = JSON.parse(results);
-
-//   //   console.log("converted search results: " + SR);
-    
-//     // for(i=0; i<results.length; i++){
-//     //   var obj = JSON.parse(results[i]);
-//     //   console.log (obj);
-//     //   var fl_item= 
-//     //     "<div class='foodlist_item'>"
-//     //     + "<a id='" + obj[i].FoodName + " herf='/foods/detail?foodName=" + obj[i].FoodName + "&FoodID=" + obj[i].foodID + ">"
-//     //     + "<img class='foodicon' src='/img/placeholder.png' alt='Food Icon'>"
-//     //     + "<div class=foodlist_itemProperty>"
-//     //     + "<ul>"
-//     //     + "<il class='foodlist_itemName'> "+ obj[i].FoodName + "</il> </br> </br>"
-//     //     + "<il> Serving Size: " + obj[i].GramsPerServing + "grams </il> </br>"
-//     //     + "<il> Calories: " + obj[i].CaloriesPerGram + " per gram </il>"
-//     //     + "</ul> </div> </a> </div>"
-//     //   $("#foodlist").append(fl_item);
-//     // }
-//    })
-//   }
-// })
-
 $(document).ready(function() {
-
-  $('#searchbtn').click(function(){
+  $('#searchbtn').click(function() { 
     let keyWord = $('#searchField').val();
-    // alert(keyWord);
+    $("#foodlist").empty();
+    searchFood(keyWord)
+  });
 
-    $.ajax({
-      url: "/api/v1/food/search/" + keyWord,
-      dataType: 'json',
-      async: false,
-      success: function(results) {
-        // alert(JSON.stringify(results));
+
+  
+});
+
+
+function searchFood(keyWord){
+
+  // alert(keyWord);
+
+  $.ajax({
+    url: "/api/v1/food/search/" + keyWord,
+    dataType: 'json',
+    async: false,
+    success: function(results) {
+      // alert(JSON.stringify(results));
+      let isEmpty = results.length == 0
+            ? true : false;
+      // alert(isEmpty);
+      if(!isEmpty){
         for(i=0; i<results.length; i++){
-          var fl_item= 
+          let fl_item= 
             "<div class='foodlist_item'>"
             + "<a id='" + results[i].FoodName + " herf='/foods/detail?foodName=" + results[i].FoodName + "&FoodID=" + results[i].foodID + ">"
             + "<img class='foodicon' src='/img/placeholder.png' alt='Food Icon'>"
@@ -55,10 +36,9 @@ $(document).ready(function() {
             + "<il> Calories: " + results[i].CaloriesPerGram + " per gram </il>"
             + "</ul> </div> </a> </div>"
           $("#foodlist").append(fl_item);
-        }
-      }
-    })
+        }  
+      } 
+      else $("#foodlist").append('Food Item Not found.');
+    }
   })
-  
-  
-});
+}
