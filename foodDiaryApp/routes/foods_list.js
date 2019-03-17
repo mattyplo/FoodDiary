2e24f1533c7b78e4815d9de144a48b501eef9ef7
YunFSui <syatwork@outlook.com>
2019-03-17 00:45:23 -0700

    add back auth to food_list

--- foodDiaryApp/routes/foods_list.js
+++ foodDiaryApp/routes/foods_list.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var auth = require('../control/auth');
 
 
-router.get('/', function(req, res, next) {
+router.get('/', auth, function(req, res, next) {
 
   res.render('foods_list', {
     title:'FOODS',
